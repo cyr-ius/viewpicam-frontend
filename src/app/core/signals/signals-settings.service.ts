efcae0214f320e0a3a10d3348cd5b415feab6130
cyr-ius <cyr-ius@ipocus.net>
2024-10-26 14:47:34 +0200

    Remove unused effect

--- src/app/core/signals/signals-settings.service.ts
+++ src/app/core/signals/signals-settings.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, signal, effect } from '@angular/core';
+import { effect, Injectable, signal } from '@angular/core';
 import { NavigationStart, Router } from '@angular/router';
 
 @Injectable({
